39de09cf13ccc173a2fb9ab75ff1ecd7b7134eae
Olga Viholainen <olga.viholainen@helsinki.fi>
2018-09-21 14:47:25 +0300

    added registeration

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -7,3 +7,11 @@ class LoginForm(FlaskForm):
 
     class Meta:
         csrf = False
+
+class CreateAccountForm(FlaskForm):
+    name = StringField("Name", [validators.Length(min=2)])
+    username = StringField("Username", [validators.Length(min=2)])  
+    password = PasswordField("Password", [validators.Length(min=2)]) 
+
+    class Meta:
+        csrf = False     
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,17 +1,17 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm
+from application.auth.forms import CreateAccountForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
     if request.method == "GET":
         return render_template("auth/loginform.html", form = LoginForm())
-
+  
     form = LoginForm(request.form)
-    # lisää validointeja 
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
@@ -24,4 +24,29 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return redirect(url_for("index"))                                   
+    return redirect(url_for("index"))
+
+@app.route("/auth/createform/")
+def auth_form():
+    return render_template("auth/registform.html", form2 = CreateAccountForm())
+
+@app.route("/auth/create/", methods=["POST"])
+def auth_create():
+    form2 = CreateAccountForm(request.form)
+
+    if not form2.validate():
+        return render_template("auth/registform.html", form2 = form2)
+
+
+    u = User(form2.name.data, form2.username.data, form2.password.data)
+
+    sameuser = User.query.filter_by(username=form2.username.data).first()
+    if sameuser:
+        return render_template("auth/registform.html", form2 = form2,
+                                error = "Username must be unique")
+
+    db.session().add(u)
+    db.session().commit()
+  
+    return redirect(url_for("naturesites_index"))
+                               
