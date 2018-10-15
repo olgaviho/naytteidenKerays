0c5558044743dd0a05e9f7a98e8a7c8973ecc2f8
Olga Viholainen <olga.viholainen@helsinki.fi>
2018-10-15 16:35:23 +0300

    removed one method

--- application/views.py
+++ application/views.py
@@ -4,5 +4,5 @@ from application.auth.models import User
 
 @app.route("/")
 def index():
-    return render_template("index.html", users_and_reports=User.how_many_reports_users_have_created())
+    return render_template("index.html")
     
